36423874b3076b09deac9a63c034b24abe93ab73
Edward Banner <edward.banner@gmail.com>
2025-02-28 14:17:11 -0500

    Add wiby URL

--- daily-slash-command/lambda_function_background.py
+++ daily-slash-command/lambda_function_background.py
@@ -1,7 +1,7 @@
 import os
-import base64
-import urllib.parse
 import json
+
+import re
 
 from datetime import datetime, timedelta
 
@@ -12,6 +12,8 @@ import requests
 import asyncio
 import aiohttp
 
+from emojis import EMOJIS
+
 
 session = aiohttp.ClientSession()
 
@@ -19,15 +21,26 @@ session = aiohttp.ClientSession()
 async def get_wordle_lotd():
     LOTD_URL = 'https://rkigvsdewg.execute-api.us-east-1.amazonaws.com/default/ygyl-python3-9/wordle-lotd'
     async with session.get(LOTD_URL) as response:
-    	wordle_lotd = await response.text()
+        wordle_lotd = await response.text()
     return wordle_lotd
+
+
+async def get_wiby_url():    
+    WIBY_URL = 'https://wiby.me/surprise'
+    async with session.get(WIBY_URL) as response:
+        wiby_text = await response.text()
+    
+    pattern = r"URL='(.*?)'"
+    match = re.search(pattern, wiby_text)
+    wiby_url = match.group(1)
+    return wiby_url
 
 
 async def get_random_word():
     wordnik_api_key = os.environ['WORDNIK_API_KEY']
-    WORDNIK_URL = f'https://api.wordnik.com/v4/words.json/randomWord?minLength=5&maxLength=5&api_key={wordnik_api_key}'
+    WORDNIK_URL = f'https://api.wordnik.com/v4/words.json/randomWord?minLength=5&maxLength=5&minCorpusCount=1&minDictionaryCount=1&api_key={wordnik_api_key}'
     async with session.get(WORDNIK_URL) as response:
-    	result = await response.json()
+        result = await response.json()
     random_word = result['word'].upper()
     return random_word
 
@@ -42,37 +55,20 @@ async def get_today_event_str():
     month, day = get_day()
     api_url = 'https://api.api-ninjas.com/v1/historicalevents?month={}&day={}'.format(month, day)
     async with session.get(api_url, headers={'X-Api-Key': os.environ['API_NINJAS_KEY']}) as response:
-    	today_events = await response.json()
+        today_events = await response.json()
     today_event = random.choice(today_events)
     year = int(today_event['year'])
 
     suffix = None
     if year < 0:
         suffix = ' BC'
+        year *= -1
     elif 0 <= year < 1000:
         suffix = ' AD'
     else:
         suffix = ''
     
-    return f"On this day {today_event['year']}{suffix}: {today_event['event']}"
-
-
-def get_response_url(event):
-    body_base64_encoded = event['body']
-    body_bytes = base64.b64decode(body_base64_encoded)
-    body_decoded = body_bytes.decode('utf-8')
-    body_dict = dict(urllib.parse.parse_qsl(body_decoded))
-    response_url = body_dict['response_url']
-    return response_url
-    
-    
-def get_slash_text(event):
-    body_base64_encoded = event['body']
-    body_bytes = base64.b64decode(body_base64_encoded)
-    body_decoded = body_bytes.decode('utf-8')
-    body_dict = dict(urllib.parse.parse_qsl(body_decoded))
-    text = body_dict.get('text', '')
-    return text
+    return f"On this day {year}{suffix}: {today_event['event']}"
 
 
 def get_abhay_blocks():
@@ -81,78 +77,106 @@ def get_abhay_blocks():
         date_time_est = date_time_utc - timedelta(hours=4)
         formatted_date = date_time_est.strftime("%a %-m/%-d/%y")
         return formatted_date
+        
+    loop = asyncio.get_event_loop()
+    today_event_str = loop.run_until_complete(get_today_event_str())
+    
+    random_wordle_emoji = random.choice(['wordle', 'wordle2'])
+    random_connections_emoji = random.choice(['connections', 'connections2'])
+    random_mini_emoji = random.choice(['mini', 'mini2', 'mini3'])
 
     date = get_date()
     abhay_blocks = [
-		{
-			"type": "rich_text",
-			"elements": [
-				{
-					"type": "rich_text_section",
-					"elements": [
-						{
-							"type": "emoji",
-							"name": "wordle2"
-						},
-						{
-							"type": "text",
-							"text": " "
-						},
-						{
-							"type": "emoji",
-							"name": "connections2"
-						},
-						{
-							"type": "text",
-							"text": " "
-						},
-						{
-							"type": "emoji",
-							"name": "mini"
-						},
+        {
+            "type": "rich_text",
+            "elements": [
+                {
+                    "type": "rich_text_section",
+                    "elements": [
                         {
-							"type": "text",
-							"text": " "
-						},
-						{
-							"type": "emoji",
-							"name": "plusword"
-						},
+                            "type": "emoji",
+                            "name": "wordle2",
+                        },
                         {
-							"type": "text",
-							"text": " "
-						},
+                            "type": "text",
+                            "text": " "
+                        },
                         {
-							"type": "text",
-							"text": date,
+                            "type": "emoji",
+                            "name": "connections2"
+                        },
+                        {
+                            "type": "text",
+                            "text": " "
+                        },
+                        {
+                            "type": "emoji",
+                            "name": "mini",
+                        },
+                        {
+                            "type": "text",
+                            "text": " "
+                        },
+                        {
+                            "type": "emoji",
+                            "name": "plusword"
+                        },
+                        {
+                            "type": "text",
+                            "text": " "
+                        },
+                        {
+                            "type": "text",
+                            "text": date,
                             "style": {
                                 "bold": True
                             }
-						},
-					]
-				}
-			]
-		}
+                        },
+                    ]
+                }
+            ]
+        },
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "type": "mrkdwn",
+                    "text": today_event_str,
+                }
+            ]
+        }
     ]
     return abhay_blocks
+
+
+def get_emoji_name():
+    emoji_name = random.choice(EMOJIS)
+    return emoji_name
 
 
 def get_eddie_blocks():
     def get_date():
         date_time_utc = datetime.now()
         date_time_est = date_time_utc - timedelta(hours=4)
-        formatted_date = date_time_est.strftime("%B %d, %Y")
+        formatted_date = date_time_est.strftime("%A\n%B %d, %Y")
         return formatted_date
     
     date = get_date()
     loop = asyncio.get_event_loop()
-    today_event_str, random_word, wordle_lotd = loop.run_until_complete(
-    	asyncio.gather(
-    		get_today_event_str(),
-    		get_random_word(),
-    		get_wordle_lotd()
-    	)
+    print('Making API calls')
+    today_event_str, random_word, wordle_lotd, wiby_url = loop.run_until_complete(
+        asyncio.gather(
+            get_today_event_str(),
+            get_random_word(),
+            get_wordle_lotd(),
+            get_wiby_url()
+        )
     )
+    print('wiby_url', wiby_url)
+    print('Made API calls')
+    emoji_name = get_emoji_name()
+    
+    random_mini_emoji = random.choice(['mini', 'mini2', 'mini3'])
     
     eddie_blocks = [
         {
@@ -162,84 +186,108 @@ def get_eddie_blocks():
                 "text": "The Dailies"
             }
         },
-		{
-			"type": "rich_text",
-			"elements": [
-				{
-					"type": "rich_text_section",
-					"elements": [
-						{
-							"type": "emoji",
-							"name": "wordle2"
-						},
-						{
-							"type": "text",
-							"text": " "
-						},
-						{
-							"type": "emoji",
-							"name": "connections2"
-						},
-						{
-							"type": "text",
-							"text": " "
-						},
-						{
-							"type": "emoji",
-							"name": "mini"
-						},
+        {
+            "type": "rich_text",
+            "elements": [
+                {
+                    "type": "rich_text_section",
+                    "elements": [
                         {
-							"type": "text",
-							"text": " "
-						},
-						{
-							"type": "emoji",
-							"name": "plusword"
-						}
-					]
-				}
-			]
-		},
-		{
-			"type": "section",
-			"text": {
-				"text": date,
-				"type": "mrkdwn"
-			},
-			"fields": [
-				{
-					"type": "mrkdwn",
-					"text": "*Random Word*"
-				},
-				{
-					"type": "plain_text",
-					"text": random_word
-				},
-				{
-					"type": "mrkdwn",
-					"text": "*LOTD*"
-				},
-				{
-					"type": "plain_text",
-					"text": wordle_lotd
-				}
-			]
-		},
-		{
-			"type": "context",
-			"elements": [
-				{
-					"type": "mrkdwn",
-					"text": today_event_str,
-				}
-			]
-		}
+                            "type": "emoji",
+                            "name": "wordle2",
+                        },
+                        {
+                            "type": "text",
+                            "text": " "
+                        },
+                        {
+                            "type": "emoji",
+                            "name": "connections2",
+                        },
+                        {
+                            "type": "text",
+                            "text": " "
+                        },
+                        {
+                            "type": "emoji",
+                            "name": random_mini_emoji,
+                        },
+                        {
+                            "type": "text",
+                            "text": " "
+                        },
+                        {
+                            "type": "emoji",
+                            "name": "plusword"
+                        },
+      #                  {
+                        #     "type": "text",
+                        #     "text": " "
+                        # },
+                        # {
+                        #     "type": "emoji",
+                        #     "name": emoji_name,
+                        # },
+                    ]
+                }
+            ]
+        },
+        {
+            "type": "section",
+            "text": {
+                "text": date,
+                "type": "mrkdwn"
+            },
+            "fields": [
+                {
+                    "type": "mrkdwn",
+                    "text": "*Word*"
+                },
+                {
+                    "type": "plain_text",
+                    "text": random_word
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": "*Letter*"
+                },
+                {
+                    "type": "plain_text",
+                    "text": wordle_lotd
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": "*Emoji*"
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": f':{emoji_name}:',
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": "*wiby*"
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": wiby_url,
+                },
+            ]
+        },
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "type": "mrkdwn",
+                    "text": today_event_str,
+                }
+            ]
+        }
     ]
     return eddie_blocks
     
     
 def get_thread_blocks(slash_text):
-    if slash_text == 'abhay':
+    if slash_text == 'abhay' or slash_text == 'smol':
         blocks = get_abhay_blocks()
     else:
         blocks = get_eddie_blocks()
@@ -247,15 +295,19 @@ def get_thread_blocks(slash_text):
 
 
 def lambda_handler(event, context):
-    response_url = get_response_url(event)
-    slash_text = get_slash_text(event)
+    slack_response_url = event['slack_response_url']
+    slash_text = event['slack_slash_text']
     
+    print('Getting thread blocks')
     data = get_thread_blocks(slash_text)
+    print('Got thread blocks')
+    print('Posting to slack response url')
     response = requests.post(
-        response_url,
+        slack_response_url,
         headers={"Content-Type": "application/json" },
         data=json.dumps(data)
     )
+    print('Posted to slack response url')
     
     print(response.status_code)
     print(response.text)
