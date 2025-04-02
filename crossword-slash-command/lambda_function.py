17b194e6f66cd11343fa9569902022c35caa7beb
Edward Banner <edward.banner@gmail.com>
2025-04-02 10:24:14 -0400

    Fix formatting

--- crossword-slash-command/lambda_function.py
+++ crossword-slash-command/lambda_function.py
@@ -16,14 +16,14 @@ def get(key, bucket='storage9'):
     value = object['Body'].read().decode('utf-8')
     return value
 
-# SLACK_BOT_TOKEN = os.environ['EDWARDS_SLACKBOT_DEV_SCK_BOT_TOKEN']
+# SLACK_BOT_TOKEN = os.environ['EDWARDS_SLACKBOT_DEV_SLACK_BOT_TOKEN']
 SLACK_BOT_TOKEN = os.environ['AWAKENED_SLACK_BOT_TOKEN']
 slack_client = WebClient(SLACK_BOT_TOKEN)
 
 def send_message(user):
     response = slack_client.chat_postMessage(
         channel='crossword-alerts',
-        text=f"{user} is playing the crossword!"
+        text=f"{user} is playing :wapo:"
     )
 
 def get_slash_text(event):
@@ -36,18 +36,199 @@ def get_slash_text(event):
     text = body_dict.get('text', '')
     return text
 
+def get_body_dict(event):
+    if 'body' not in event:
+        return None
+    body_base64_encoded = event['body']
+    body_bytes = base64.b64decode(body_base64_encoded)
+    body_decoded = body_bytes.decode('utf-8')
+    body_dict = dict(urllib.parse.parse_qsl(body_decoded))
+    return body_dict
+
+
+def get_user_agent(event):
+    headers = event.get('headers')
+    if headers:
+        return headers.get('user-agent')
+    return None
+
 
 def lambda_handler(event, context):
+    user_agent = get_user_agent(event)
+    if user_agent == 'Slackbot-LinkExpanding 1.0 (+https://api.slack.com/robots)':
+        print('MATCHED USER_AGENT')
+        return {
+            "statusCode": 200
+        }
+    else:
+        print(f"DIDN'T MATCH USER_AGENT: {user_agent}")
+
     url = get_slash_text(event)
     if url:
         put('wapo-url', url)
         lambda_url = os.environ["LAMBDA_URL"]
+
+        # message_blocks = {
+        #     "blocks" [
+        #         {
+        # "type": "rich_text",
+        # "block_id": "block1",
+        # "elements": [
+        #     {
+        #     "type": "rich_text_section",
+        #     "elements": [
+        #         {
+        #         "type": "text",
+        #         "text": "My favorite Slack features (in no particular order):"
+        #         }
+        #     ]
+        #     },
+        #     {
+        #     "type": "rich_text_list",
+        #     "elements": [
+        #         {
+        #         "type": "rich_text_section",
+        #         "elements": [
+        #             {
+        #             "type": "text",
+        #             "text": "Huddles"
+        #             }
+        #         ]
+        #         },
+        #         {
+        #         "type": "rich_text_section",
+        #         "elements": [
+        #             {
+        #             "type": "text",
+        #             "text": "Canvas"
+        #             }
+        #         ]
+        #         },
+        #         {
+        #         "type": "rich_text_section",
+        #         "elements": [
+        #             {
+        #             "type": "text",
+        #             "text": "Developing with Block Kit"
+        #             }
+        #         ]
+        #         }
+        #     ],
+        #     "style": "bullet",
+        #     "indent": 0,
+        #     "border": 1
+        #     },
+        # ],
+        # }]
+        # }
+
+        # message_blocks = {
+        #     "blocks": [
+        #         {
+        #             "type": "section",
+        #             "text": {
+        #                 "type": "mrkdwn",
+        #                 "text": f"<{url}|Collab crossword>"
+        #             }
+        #         },
+        #         {
+        #             "type": "section",
+        #             "text": {
+        #                 "type": "mrkdwn",
+        #                 "text": f"• <{lambda_url}/eddie|Eddie link>\n• <{lambda_url}/katherine|Katherine link>\n• <{lambda_url}/abhay|Abhay link>"
+        #             }
+        #         }
+        #     ]
+        # }
+
         message_blocks = {
             "blocks": [
-                {"type": "section", "text": {"type": "mrkdwn", "text": f"<{url}|Collab crossword>"}},
-                {"type": "section", "text": {"type": "mrkdwn", "text": f"• <{lambda_url}/eddie|Eddie link>\n• <{lambda_url}/katherine|Katherine link>\n• <{lambda_url}/abhay|Abhay link>"}}
+                {
+                    "type": "rich_text",
+                    "block_id": "block1",
+                    "elements": [
+                        {
+                            "type": "rich_text_section",
+                            "elements": [
+                                {
+                                    "type": "link",
+                                    "url": url,
+                                    "text": "Collab crossword"
+                                }
+                            ]
+                        },
+                        {
+                            "type": "rich_text_section",
+                            "elements": [
+                                {
+                                    "type": "text",
+                                    "text": " "
+                                }
+                            ]
+                        },
+                        {
+                            "type": "rich_text_list",
+                            "style": "bullet",
+                            "elements": [
+                                {
+                                    "type": "rich_text_section",
+                                    "elements": [
+                                        {
+                                            "type": "link",
+                                            "url": f"{lambda_url}/eddie",
+                                            "text": "Eddie link"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "rich_text_section",
+                                    "elements": [
+                                        {
+                                            "type": "link",
+                                            "url": f"{lambda_url}/katherine",
+                                            "text": "Katherine link"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "rich_text_section",
+                                    "elements": [
+                                        {
+                                            "type": "link",
+                                            "url": f"{lambda_url}/abhay",
+                                            "text": "Abhay link"
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                    ],
+                }
             ]
         }
+
+
+
+        # message_blocks = {
+        #     "blocks" [
+        #         {
+        #             "type": "rich_text",
+        #             "block_id": "block1",
+        #             "elements": [
+        #                 {
+        #                     "type": "rich_text_section",
+        #                     "elements": [
+        #                         {
+        #                             "type": "text",
+        #                             "text": "My favorite Slack features (in no particular order):"
+        #                         }
+        #                     ]
+        #                 },
+        #             ],
+        #         }
+        #     ]
+        # }
+
         return {
             "statusCode": 200,
             "body": json.dumps(message_blocks),
